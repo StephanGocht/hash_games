b1f2d783e6fd22489dc832d6855478a3b4aaeabf
Stephan Gocht <stephan@gobro.de>
2021-04-23 19:53:41 +0200

    Add working md5.

--- sha2pb.py
+++ sha2pb.py
@@ -133,33 +133,29 @@ class CNFFormula(Formula):
         return "%i"%(i)
 
     def _isEnd(self, char):
-        return char == ";"
+        return char == "0"
 
     def _header(self):
         self.print("p cnf %i %i" % (self.maxVar, self.nConstraints), end = "")
 
-
-class CNFBits(object):
-    def __init__(self, formula, num_chunks):
-        self.inputVars = None
+class VarBits(object):
+    def __init__(self, num_chunks):
         self.num_chunks = num_chunks
-        self.formula = formula
-
-        # reserve space for header
-        self.print(" " * 100)
-
-    def newVars(self, n):
-        return self.formula.newVars(n)
-
-    def print(self, *args, **kwargs):
-        self.formula.print(*args,**kwargs)
+        self.inputVars = None
 
     def getInBits(self):
         if self.inputVars is None:
             self.inputVars = self.newVars(512 * self.num_chunks)
         return self.inputVars
 
-    def blocks(self):
+    def toLittleEnd(self, value, nbytes = 4):
+        assert(len(value) == nbytes * 8)
+        littleEndian = []
+        for i in range(nbytes):
+            littleEndian.extend([value[j-(i+1)*8] for j in range(8)])
+        return littleEndian
+
+    def blocks(self, byteorder = 'big'):
         bits = self.getInBits()
         self.data = iter(bits)
 
@@ -167,19 +163,43 @@ class CNFBits(object):
             w = []
             for i in range(16):
                 try:
-                    w.append([next(self.data) for i in range(32)])
+                    nextInt32 = [next(self.data) for i in range(32)]
                 except StopIteration:
                     return
+
+                if byteorder == 'little':
+                    nextInt32 = self.toLittleEnd(nextInt32)
+                w.append(nextInt32)
+
             yield w
 
     def leftrotate(self, n, value):
         return n[value:] + n[:value]
 
     def rightrotate(self, n, value):
-        return n[:value] + n[value:]
+        nBits = len(n)
+        return n[nBits - value:] + n[:nBits - value]
 
     def rightshift(self, n, value):
-        return ([self.zero] * value) + n[:value]
+        nBits = len(n)
+        return ([self.zero] * value) + n[:nBits - value]
+
+
+
+class CNFBits(VarBits):
+    def __init__(self, formula, num_chunks):
+        super().__init__(num_chunks)
+        self.formula = formula
+
+        # reserve space for header
+        self.print(" " * 100)
+        self.zero = self.constant(0,1)[0]
+
+    def newVars(self, n):
+        return self.formula.newVars(n)
+
+    def print(self, *args, **kwargs):
+        self.formula.print(*args,**kwargs)
 
     def bitwise_if(self, b, c, d):
         # (b & c) | ((~b) & d)
@@ -235,8 +255,8 @@ class CNFBits(object):
     def getAdderMap(self):
         if self.adderMap is None:
             self.adderMap = dict()
-            for i,j in [(2,2), (3,2), (5,3), (6,3), (7,3)]:
-                fileName = "sha1-sat/data/halfadder-%i-%i.out.txt"
+            for i,j in [(2,2), (3,2), (4,3), (5,3), (6,3), (7,3)]:
+                fileName = "data/halfadder-%i-%i.out.txt"
                 with open(fileName%(i,j), "r") as file:
                     res = list()
                     for line in file:
@@ -319,6 +339,29 @@ class CNFBits(object):
 
         return result
 
+    def bitwise_md5_i(self, b, c, d):
+        # return c ^ (b | ~d)
+        assert(len(b) == len(c) and len(c) == len(d))
+        res = self.newVars(len(b))
+
+        for x,y,z,f in zip(b,c,d,res):
+            clauses = [
+                [(1,f), (1,x), (1,y), (1,z)],
+                [(1,f), (1,x), (0,y), (0,z)],
+                [(1,f), (0,x), (1,y), (1,z)],
+                [(1,f), (0,x), (1,y), (0,z)],
+                [(0,f), (1,x), (1,y), (0,z)],
+                [(0,f), (1,x), (0,y), (1,z)],
+                [(0,f), (0,x), (0,y), (1,z)],
+                [(0,f), (0,x), (0,y), (0,z)],
+            ]
+            for clause in clauses:
+                for sign, var in clause:
+                    self.print("%s%s "%("-" if not sign else "", var), end = "")
+                self.print("0")
+
+        return res
+
     def constant(self, value, nBits):
         result = self.newVars(nBits)
         self.setVars(value, result)
@@ -334,16 +377,15 @@ class CNFBits(object):
             self.print("%s%s 0"%("" if val else "-", var))
 
 
-class PBBits(object):
+class PBBits(VarBits):
     def __init__(self, formula, num_chunks):
-        self.num_chunks = num_chunks
-        self.inputVars = None
+        super().__init__(num_chunks)
         self.formula = formula
 
         # reserve space for header
         self.print(" " * 100)
 
-        self.zero = self.constant(0,1)
+        self.zero = self.constant(0,1)[0]
 
     def newVars(self, n):
         return self.formula.newVars(n)
@@ -351,44 +393,6 @@ class PBBits(object):
     def print(self, *args, **kwargs):
         self.formula.print(*args,**kwargs)
 
-    def getInBits(self):
-        if self.inputVars is None:
-            self.inputVars = self.newVars(512 * self.num_chunks)
-        return self.inputVars
-
-    def toLittleEnd(self, value, nbytes = 4):
-        assert(len(value) == nbytes * 8)
-        littleEndian = []
-        for i in range(nbytes):
-            littleEndian.extend([value[j-(i+1)*8] for j in range(8)])
-        return littleEndian
-
-    def blocks(self, byteorder = 'big'):
-        bits = self.getInBits()
-        self.data = iter(bits)
-
-        while True:
-            w = []
-            for i in range(16):
-                try:
-                    nextInt32 = [next(self.data) for i in range(32)]
-                except StopIteration:
-                    return
-
-                if byteorder == 'little':
-                    nextInt32 = self.toLittleEnd(nextInt32)
-                w.append(nextInt32)
-
-            yield w
-
-    def leftrotate(self, n, value):
-        return n[value:] + n[:value]
-
-    def rightrotate(self, n, value):
-        return n[:value] + n[value:]
-
-    def rightshift(self, n, value):
-        return ([self.zero] * value) + n[:value]
 
     def bitwise_if(self, b, c, d):
         # (b & c) | ((~b) & d)
@@ -466,6 +470,29 @@ class PBBits(object):
                 self.print("-2 %s " % (var), end = "")
 
             self.print("= 0 ;")
+
+        return res
+
+    def bitwise_md5_i(self, b, c, d):
+        # return c ^ (b | ~d)
+        assert(len(b) == len(c) and len(c) == len(d))
+        res = self.newVars(len(b))
+
+        for x,y,z,f in zip(b,c,d,res):
+            clauses = [
+                [(1,f), (1,x), (1,y), (1,z)],
+                [(1,f), (1,x), (0,y), (0,z)],
+                [(1,f), (0,x), (1,y), (1,z)],
+                [(1,f), (0,x), (1,y), (0,z)],
+                [(0,f), (1,x), (1,y), (0,z)],
+                [(0,f), (1,x), (0,y), (1,z)],
+                [(0,f), (0,x), (0,y), (1,z)],
+                [(0,f), (0,x), (0,y), (0,z)],
+            ]
+            for clause in clauses:
+                for sign, var in clause:
+                    self.print("1 %s%s "%("~" if not sign else "", var), end = "")
+                self.print(">= 1 ;")
 
         return res
 
@@ -824,9 +851,7 @@ class HashHandler:
 
         nBits = len(inBits)
         inbytes = indata.to_bytes(nBits // 8, byteorder = 'big')
-        print(inbytes)
         message_length = int.from_bytes(inbytes[-8:], byteorder = self.byteorder)
-        print("ml", message_length)
 
         if outputPreImage:
             outputPreImage.write(inbytes[:message_length // 8])
@@ -855,43 +880,9 @@ def myhex(value, bits):
     frmt = "%0{0:d}x".format(hexdigits)
     return (frmt % (value))
 
-def main():
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("hash_function", choices = ["sha1","sha256","md4","md5"])
-
-    parser.add_argument("--format", "-f", choices=["cnf", "opb"], default="opb")
-    parser.add_argument("--seed", type=int)
-
-    subparsers = parser.add_subparsers(dest='what')
-    solutionParser = subparsers.add_parser("solution")
-    solutionParser.add_argument("solutionFile", type=argparse.FileType("rt"),
-        default = "sol.txt")
-    solutionParser.add_argument("outputPreImage", type=argparse.FileType("wb"),
-        default = "result_file.txt",
-        nargs = "?")
-
-    solveParser = subparsers.add_parser("solve")
-    solveParser.add_argument("outputFormula", type=argparse.FileType("wt"))
-    solveParser.add_argument("fileToHash", type=argparse.FileType("rb"), nargs = "?")
-    solveParser.add_argument("--messageBytes", type=int, default=55)
-    solveParser.add_argument("--fixedInBits", type=int)
-    solveParser.add_argument("--fixedInBitsStart", type=int)
-
-    solveParser = subparsers.add_parser("analyze")
-    solveParser.add_argument("outputFormula", type=argparse.FileType("wt"))
-    solveParser.add_argument("fileToHash", type=argparse.FileType("rb"), nargs = "?")
-    solveParser.add_argument("--messageBytes", type=int, default=55)
-
-    computeParser = subparsers.add_parser("compute")
-    computeParser.add_argument("fileToHash", type=argparse.FileType("rb"))
-
-    args = parser.parse_args()
-
-    if args.seed:
-        random.seed(args.seed)
-    random.seed(1)
+def run(args):
+    if args["seed"]:
+        random.seed(args["seed"])
 
     algs = {
         "sha1": (sha1, 'big'),
@@ -899,23 +890,23 @@ def main():
         "md4": (md4, 'little'),
         "md5": (md5, 'little')
     }
-    hashing = HashHandler(*algs[args.hash_function])
+    hashing = HashHandler(*algs[args["hash_function"]])
 
-    if args.what == "compute":
-        data = bytearray(args.fileToHash.read())
+    if args["what"] == "compute":
+        data = bytearray(args["fileToHash"].read())
         data = hashing.prepare(data)
         bt = TrueBits(data)
         h = hashing(bt)
         result = hashing.join(h)
         print(myhex(result, len(h) * 32))
 
-    if args.what == "solve":
-        if args.fileToHash:
-            data = bytearray(args.fileToHash.read())
+    if args["what"] == "solve":
+        if args["fileToHash"]:
+            data = bytearray(args["fileToHash"].read())
             msg_bytes = len(data)
         else:
             # 55 is maximum for one chunk
-            msg_bytes = args.messageBytes
+            msg_bytes = args["messageBytes"]
             rnd = random.getrandbits(msg_bytes * 8)
             data = bytearray(rnd.to_bytes(msg_bytes, byteorder = 'big'))
 
@@ -923,13 +914,13 @@ def main():
         assert(len(data) * 8 % 512 == 0)
 
         num_chunks = len(data) * 8 // 512
-        print("number of chunks:", num_chunks)
+        # print("number of chunks:", num_chunks)
 
-        if args.format == "cnf":
-            formula = CNFFormula(args.outputFormula)
+        if args["format"] == "cnf":
+            formula = CNFFormula(args["outputFormula"])
             bt = CNFBits(formula, num_chunks)
-        elif args.format == "opb":
-            formula = OPBFormula(args.outputFormula)
+        elif args["format"] == "opb":
+            formula = OPBFormula(args["outputFormula"])
             bt = PBBits(formula, num_chunks)
 
         inBits  = bt.getInBits()
@@ -945,24 +936,24 @@ def main():
         # maximum number of free bits
         maxFreeBits = allBits - forcedBits
         # actual number of free bits
-        freeBits = 10
+        freeBits = args["nFreeInBits"]
         bits = allBits - freeBits
         assert(bits >= forcedBits)
 
         bt.setVars(int.from_bytes(data, byteorder = 'big'), inBits[-bits:])
 
-        outZeroBits = 4
+        outZeroBits = args["nZeroOutBits"]
         bt.setVars(0, outBits[:outZeroBits])
 
         formula.writeHeader()
 
-    if args.what == "analyze":
-        if args.fileToHash:
-            data = bytearray(args.fileToHash.read())
+    if args["what"] == "analyze":
+        if args["fileToHash"]:
+            data = bytearray(args["fileToHash"].read())
             msg_bytes = len(data)
         else:
             # 55 is maximum for one chunk
-            msg_bytes = args.messageBytes
+            msg_bytes = args["messageBytes"]
             rnd = random.getrandbits(msg_bytes * 8)
             data = bytearray(rnd.to_bytes(msg_bytes, byteorder = 'big'))
 
@@ -970,14 +961,14 @@ def main():
         assert(len(data) * 8 % 512 == 0)
 
         num_chunks = len(data) * 8 // 512
-        print("number of chunks:", num_chunks)
+        # print("number of chunks:", num_chunks)
 
-        if args.format == "cnf":
-            formula = CNFFormula(args.outputFormula)
+        if args["format"] == "cnf":
+            formula = CNFFormula(args["outputFormula"])
             bt1 = CNFBits(formula, num_chunks)
             bt2 = CNFBits(formula, num_chunks)
-        elif args.format == "opb":
-            formula = OPBFormula(args.outputFormula)
+        elif args["format"] == "opb":
+            formula = OPBFormula(args["outputFormula"])
             bt1 = PBBits(formula, num_chunks)
             bt2 = PBBits(formula, num_chunks)
 
@@ -1009,25 +1000,62 @@ def main():
 
         formula.writeHeader()
 
-    if args.what == "solution":
-        if args.format == "cnf":
-            solution = readSolutionCnf(args.solutionFile)
-        elif args.format == "opb":
-            solution = readSolutionOpb(args.solutionFile)
+    if args["what"] == "solution":
+        if args["format"] == "cnf":
+            solution = readSolutionCnf(args["solutionFile"])
+        elif args["format"] == "opb":
+            solution = readSolutionOpb(args["solutionFile"])
 
         num_chunks = 1
-        print("number of chunks:", num_chunks)
+        # print("number of chunks:", num_chunks)
 
-        if args.format == "cnf":
+        if args["format"] == "cnf":
             bt = CNFBits(CNFFormula(None), num_chunks)
-        elif args.format == "opb":
+        elif args["format"] == "opb":
             bt = PBBits(OPBFormula(None), num_chunks)
 
 
-        hashing.renderSolution(bt, solution, args.outputPreImage)
+        hashing.renderSolution(bt, solution, args["outputPreImage"])
 
+def main():
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument("hash_function", choices = ["sha1","sha256","md4","md5"])
 
+    parser.add_argument("--format", "-f", choices=["cnf", "opb"], default="opb")
+    parser.add_argument("--seed", type=int)
+    parser.add_argument('--dumpSettings', action='store_true', help="Dump settings as dictionary and exit.")
+
+    subparsers = parser.add_subparsers(dest='what')
+    solutionParser = subparsers.add_parser("solution")
+    solutionParser.add_argument("solutionFile", type=argparse.FileType("rt"),
+        default = "sol.txt")
+    solutionParser.add_argument("outputPreImage", type=argparse.FileType("wb"),
+        default = "result_file.txt",
+        nargs = "?")
+
+    solveParser = subparsers.add_parser("solve")
+    solveParser.add_argument("outputFormula", type=argparse.FileType("wt"))
+    solveParser.add_argument("fileToHash", type=argparse.FileType("rb"), nargs = "?")
+    solveParser.add_argument("--messageBytes", type=int, default=55)
+    solveParser.add_argument("--nZeroOutBits", type=int, default=8)
+    solveParser.add_argument("--nFreeInBits", type=int, default=10)
+
+    solveParser = subparsers.add_parser("analyze")
+    solveParser.add_argument("outputFormula", type=argparse.FileType("wt"))
+    solveParser.add_argument("fileToHash", type=argparse.FileType("rb"), nargs = "?")
+    solveParser.add_argument("--messageBytes", type=int, default=55)
+
+    computeParser = subparsers.add_parser("compute")
+    computeParser.add_argument("fileToHash", type=argparse.FileType("rb"))
+
+    args = parser.parse_args()
+
+    if args.dumpSettings:
+        import pprint
+        pprint.pprint(vars(args))
+
+    run(vars(args))
 
 if __name__ == '__main__':
     main()
